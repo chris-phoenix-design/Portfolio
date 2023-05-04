cc2435147d8d1d255dea110fe38c6242ab5535c4
Christopher Buchanan <chrisb@abs-link.com>
2023-05-04 16:10:46 -0500

    update

--- js/contact.js
+++ js/contact.js
@@ -3,38 +3,34 @@ let contactForm = document.getElementById("message_form");
 async function submitForm(event) {
     debugger;
     event.preventDefault();
+    grecaptcha.execute();
     let name = contactForm["name"];
     let phone = contactForm["phone"];
     let email = contactForm["email"];
     let subject = contactForm["subject"];
     let message = contactForm["message"];
 
-    var recaptcharesponse = grecaptcha.getResponse();
-    if (recaptcharesponse.length != 0) {
+    const params = {
+        Name: name.value,
+        Phone: phone.value,
+        Subject: subject,
+        Email: email.value,
+        Message: message.value
+    };
+    const options = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain',
+            'Content-Type': 'application/json;charset=UTF-8'
+        },
+        body: JSON.stringify(params)
+    };
 
-        const params = {
-            Name: name.value,
-            Phone: phone.value,
-            Subject: subject,
-            Email: email.value,
-            Message: message.value
-        };
-        const options = {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json, text/plain',
-                'Content-Type': 'application/json;charset=UTF-8'
-            },
-            body: JSON.stringify(params)
-        };
-
-        let response = await fetch('', options);
-        if (response.ok) {
-            contactForm.reset();
-        } else {
-            contactForm.reset();
-        }
-
+    let response = await fetch('', options);
+    if (response.ok) {
+        contactForm.reset();
+    } else {
+        contactForm.reset();
     }
 }
 
