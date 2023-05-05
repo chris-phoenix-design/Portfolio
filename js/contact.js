7807f9d6d6f86b77a6f8aea098d08cbb5585e8b6
chris-phoenix-design <chris@phoenixsoftwaredesign.com>
2023-05-05 15:31:18 -0500

    update

--- js/contact.js
+++ js/contact.js
@@ -4,32 +4,52 @@ const submitForm = (event) => {
 
     event.preventDefault();
     grecaptcha.execute();
-    let name = contactForm["name"];
-    let phone = contactForm["phone"];
-    let email = contactForm["email"];
-    let subject = contactForm["subject"];
-    let message = contactForm["message"];
+    let name = contactForm["name"].value;
+    let phone = contactForm["phone"].value;
+    let email = contactForm["email"].value;
+    let subject = contactForm["subject"].value;
+    let message = contactForm["message"].value;
 
     const params = {
-        Name: name.value,
-        Phone: phone.value,
+        Name: name,
+        Phone: phone,
         Subject: subject,
-        Email: email.value,
-        Message: message.value
+        Email: email,
+        Message: message
     };
     const options = {
         method: 'POST',
         headers: {
             'Accept': 'application/json, text/plain',
-            'Content-Type': 'application/json;charset=UTF-8'
+            'Content-Type': 'application/json;charset=UTF-8',
+            "Access-Control-Allow-Origin": "https://localhost:7098",
+            "Access-Control-Allow-Credentials": "true"
         },
         body: JSON.stringify(params)
     };
 
-    fetch('https://api-phoenix.azurewebsites.net/api/emailsender/ProfileContact', options)
 
+    // let form = new FormData();
+    // form.append("name", name);
+    // form.append("email", email);
+    // form.append("Phone",  phone);
+    // form.append("Subject", subject);    
+    // form.append("message", message);
+    // const options = {
+    //     method: 'POST',
+    //     body: form
+    // };
+    
+let url = 'https://api-phoenix.azurewebsites.net/api/emailsender/ProfileContact';
+    //url = 'https://localhost:7098/api/emailsender/ProfileContact';
+    fetch(url, options)
+        .then(res => {
+            if (res.ok) {
+                contactForm.reset();
+            }
+            });
 
-    contactForm.reset();
+   
 
 }
 
